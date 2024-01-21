232567a3ddd746c7256a6736899343bc519d3c78
maxvanbel <maxvanbel@gmail.com>
2024-01-20 19:57:30 -0600

    Fetch data via server component!

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,8 +1,16 @@
-
-export default function Home() {
+import { db } from "@/db";
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  const renderedSnippets = snippets.map((s) => {
+    return (<div key={s.id}>
+      <h2>Title: {s.title}</h2>
+      <pre>Code: {s.code}</pre>
+    </div>)
+  });
   return (
     <div>
       <h1>Home</h1>
+      {renderedSnippets}
     </div>
-  )
+  );
 }
